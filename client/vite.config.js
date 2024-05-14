e25c04b1f138bb713db285618136587159bdaeb0
ahmed <ahmed.adnan0202@gmail.com>
2024-05-15 00:18:48 +0300

    changed vite port

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 5173,
     proxy: {
       "/api": {
         target: "http://localhost:5000",
